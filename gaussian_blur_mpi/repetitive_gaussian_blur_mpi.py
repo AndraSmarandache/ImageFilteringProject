98f4baae6cb7117c774b44e9c1cbeae08ecccee6
Andra-Maria Smarandache <andra_smarandache@yahoo.com>
2025-05-07 13:10:21 +0300

    Update repetitive_gaussian_blur_mpi.py

--- gaussian_blur_mpi/repetitive_gaussian_blur_mpi.py
+++ gaussian_blur_mpi/repetitive_gaussian_blur_mpi.py
@@ -55,7 +55,7 @@ def main():
     else:
         comm.Recv(local_data, source=0, tag=rank)
 
-    process_start = time.time() * 1000  # Convert to milliseconds
+    process_start = time.time() * 1000  
     local_result = np.empty((rows_per_process, width, 3), dtype=np.uint8)
     for c in range(3):
         for j in range(25):
@@ -71,7 +71,7 @@ def main():
             recv_buf = np.empty((other_end-other_start, width, 3), dtype=np.uint8)
             comm.Recv(recv_buf, source=i, tag=i+100)
             result[other_start:other_end] = recv_buf
-        process_end = time.time() * 1000  # Convert to milliseconds
+        process_end = time.time() * 1000 
         
         cv2.imwrite(output_path, result)
         print(f"Parallel Processing Time: {process_end-process_start:.2f}")
